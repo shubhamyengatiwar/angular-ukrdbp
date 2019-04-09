3c588542217b9e960d58c154483681c3a01e95ca
shubhamyengatiwar <yengantiwar.shubham@praxislogica.com>
2019-04-09 19:13:29 +0530

    2 files modified, 5 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,29 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
-import { HEROES } from '../mock-heroes';
-
+ 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+ 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
- selectedHero: Hero;
-
-  constructor() { }
-  hero : Hero ={
-    id : 1,
-    name : 'Shubham'
-  }
-
-  heroes = HEROES ;
-  ngOnInit() {
-  }
-
-
  
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-
+  selectedHero: Hero;
+ 
+  heroes: Hero[];
+ 
+  constructor(private heroService: HeroService) { }
+ 
+  ngOnInit() {
+    this.getHeroes();
+  }
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+ 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
